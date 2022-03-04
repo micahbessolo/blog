3fa0620fec4d49a4fd5b3f18ab75ba1b9135d8d7
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-03-03 21:16:04 -0800

    updates

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
+
 app.get('/', (req, res) =>{
-    res.render('index')
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        desription: 'Test description'
+    },
+    {
+        title: 'Test Article 2',
+        createdAt: new Date(),
+        desription: 'Test description 2'
+    }]
+    res.render('index', { articles: articles })
 })
 
 app.listen(3000)
